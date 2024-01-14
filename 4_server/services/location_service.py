e8064bda39825fd91398619d1d991faac61d8587
Aritra Ghosh <aritra119.26@gmail.com>
2024-01-14 22:47:46 +0530

    fix versions

--- 4_server/services/location_service.py
+++ 4_server/services/location_service.py
@@ -7,16 +7,31 @@ from models.location import Location
 
 
 def get_location_summary(location: str):
-    color_function = convert_pydantic_to_openai_function(Location)
+    location_function = convert_pydantic_to_openai_function(Location)
+
+    # get chat history from session
+    # chat_repository.get_history(user_id, chat_id)
+
+    # search vector db for query
+    # chat_repository.get_relevant_docs(query)
+
+    # add above data to system
 
     prompt = ChatPromptTemplate.from_template(
         """ 
         The place is {location} 
         """
     )
-    model = ChatOpenAI().bind(functions=[color_function])
+
+    # check if we can move this to config
+    model = ChatOpenAI().bind(functions=[location_function])
     chain = prompt | model | JsonOutputFunctionsParser()
-    return chain.invoke({
+
+    # we can create agent here in case function call is required
+    res = chain.invoke({
         "location": location
     })
+
+    # save response to chat history
+    return res
 
