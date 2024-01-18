f1705c285752a048f4cacf32ec9248763682820a
Aritra Ghosh <aritra119.26@gmail.com>
2024-01-18 08:34:56 +0530

    fix: remove unnecessary code comments

--- 3_vector_store/vector_store.py
+++ 3_vector_store/vector_store.py
@@ -5,31 +5,31 @@ from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 
-# loader = TextLoader('../resources/info.txt', encoding='utf-8')
-# documents = loader.load()
-# print(len(documents))
-#
-# text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=80)
-# texts = text_splitter.split_documents(documents)
-#
-# print(len(texts))
+loader = TextLoader('../resources/info.txt', encoding='utf-8')
+documents = loader.load()
+print(len(documents))
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=80)
+texts = text_splitter.split_documents(documents)
+
+print(len(texts))
 #
 embeddings = OpenAIEmbeddings()
-# doc_vectors = embeddings.embed_documents([t.page_content for t in texts])
-#
-# print(len(doc_vectors))
-# print(len(doc_vectors[0]))
+doc_vectors = embeddings.embed_documents([t.page_content for t in texts])
+
+print(len(doc_vectors))
+print(len(doc_vectors[0]))
 
 pg_connection_string = "postgresql+psycopg2://postgres:mysecretpassword@localhost:5432/postgres"
 collection_name = "learning"
-#
-# # we are filling entries in the database here
-# db = PGVector.from_documents(
-#     embedding=embeddings,
-#     documents=texts,
-#     collection_name=collection_name,
-#     connection_string=pg_connection_string
-# )
+
+# we are filling entries in the database here
+db = PGVector.from_documents(
+    embedding=embeddings,
+    documents=texts,
+    collection_name=collection_name,
+    connection_string=pg_connection_string
+)
 
 # if you want to just connect to the database for querying etc
 db_existing = PGVector(
@@ -47,49 +47,49 @@ content = db_existing.similarity_search("What are fields in computer science?", 
 })
 print(content)
 
-# content = db.similarity_search("What are fields in computer science?", k=10)
-# model = ChatOpenAI(temperature=0)
-# prompt = ChatPromptTemplate.from_messages([
-#     ("system", """
-#         You are a professor in computer science.
-#         {context}
-#     """),
-#     ("user", "{input}"),
-# ])
-#
-# print('**************************************')
-# print(model.invoke(
-#     [
-#         SystemMessage(
-#             content=[
-#                 {"type": "text", "text": "You are a professor in computer science"
-#                                          + ','.join([c.page_content for c in content])}
-#             ]
-#         ),
-#         HumanMessage(
-#             content=[
-#                 {"type": "text", "text": """
-#                 What are fields in computer science?
-#                 Can you mention them in a list?
-#                 Do not give any other text.
-#                 """},
-#             ]
-#         )
-#     ]
-# ))
-#
-# print('**************************************')
-# message = {
-#     "context": ','.join([c.page_content for c in content]),
-#     "input": """
-#         What are fields in computer science?
-#         Can you mention them in a list?
-#         Do not give any other text.
-#     """
-# }
-# print(prompt.invoke(message))
-#
-# chain = prompt | model
-#
-# print('**************************************')
-# print(chain.invoke(message))
+content = db.similarity_search("What are fields in computer science?", k=10)
+model = ChatOpenAI(temperature=0)
+prompt = ChatPromptTemplate.from_messages([
+    ("system", """
+        You are a professor in computer science.
+        {context}
+    """),
+    ("user", "{input}"),
+])
+
+print('**************************************')
+print(model.invoke(
+    [
+        SystemMessage(
+            content=[
+                {"type": "text", "text": "You are a professor in computer science"
+                                         + ','.join([c.page_content for c in content])}
+            ]
+        ),
+        HumanMessage(
+            content=[
+                {"type": "text", "text": """
+                What are fields in computer science?
+                Can you mention them in a list?
+                Do not give any other text.
+                """},
+            ]
+        )
+    ]
+))
+
+print('**************************************')
+message = {
+    "context": ','.join([c.page_content for c in content]),
+    "input": """
+        What are fields in computer science?
+        Can you mention them in a list?
+        Do not give any other text.
+    """
+}
+print(prompt.invoke(message))
+
+chain = prompt | model
+
+print('**************************************')
+print(chain.invoke(message))
--- 4_server/services/location_service.py
+++ 4_server/services/location_service.py
@@ -2,35 +2,58 @@ from langchain.agents import AgentExecutor
 from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
 from langchain.prompts import ChatPromptTemplate
 from langchain.tools.render import format_tool_to_openai_function
+from langchain_core.messages import messages_from_dict
+from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 
+from config.common import summary_prompt
+from repository.chat_history_repository import load_chat_history
+from repository.knowledge_repository import query_documents
 from tools.location import get_location_coordinates
 
 
-def get_location_summary(query: str):
+# keeping this here in case specific summary is needed
+def _summarize_chat_history(chat_id: str) -> str:
+    messages = load_chat_history(chat_id)
+    chat_history = messages_from_dict(messages)
+    model = ChatOpenAI(model_name="gpt-4-1106-preview")
+    summary_chain = summary_prompt | model | StrOutputParser()
+    chat_summary = summary_chain.invoke({"context": chat_history})
+    return chat_summary
+
+
+def get_location_summary(query: str) -> str:
     location_function = format_tool_to_openai_function(get_location_coordinates)
 
-    # get chat history from session
-    # chat_repository.get_history(user_id, chat_id)
+    # get chat history from session, hardcoding session for easy understanding
+    # get chat id from user info using middleware
+    chat_summary = _summarize_chat_history("1")
 
-    # search vector db for query
-    # chat_repository.get_relevant_docs(query)
+    # search vector db for query, hardocoding collection name for easy understanding
+    # get collection name based on use case
+    reference_docs = query_documents("learning", query)
 
-    # add above data to system
-
+    # add above data to system context
+    # have use case specific prompt
     prompt = ChatPromptTemplate.from_messages([
         (
             "system", """
                 You are a helpful assistant. 
                 If your answer has multiple lines, please format it in bullet points. 
                 Use html formatting for response in that case if it's not a tool call.
+                
+                Use this for reference:
+                {reference_docs}
+                
+                Here is the previous chat history:
+                {chat_history}
             """
         ),
         ("user", "{query}"),
     ])
 
     # check if we can move this to config
-    model = ChatOpenAI(model_name="gpt-4-1106-preview")\
+    model = ChatOpenAI(model_name="gpt-4-1106-preview") \
         .bind(functions=[location_function])
     chain = prompt | model | OpenAIFunctionsAgentOutputParser()
     agent_executor = AgentExecutor(agent=chain, tools=[
@@ -38,8 +61,10 @@ def get_location_summary(query: str):
     ], verbose=True)
     # we can create agent here in case function call is required
     res = agent_executor.invoke({
-        "query": query
+        "query": query,
+        "reference_docs": reference_docs,
+        "chat_history": chat_summary
     })
 
     # save response to chat history
-    return res
+    return res['output']
